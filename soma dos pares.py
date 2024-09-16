435141836cddfc2a57f24d14d105be4d352f7971
GustavoSantanaS <gustavossantanasantos@gmail.com>
2024-09-16 16:52:10 -0300

    soma dos pares.py

--- soma dos pares.py
+++ soma dos pares.py
@@ -3,5 +3,5 @@ soma_pares = 0
 
 for i in range(1, numero + 1):
     if i % 2 == 0:
-        soma_pares += i
+        soma_pares = soma_pares + i
 print("A soma dos números pares entre 1 e", numero, "é:", soma_pares)
