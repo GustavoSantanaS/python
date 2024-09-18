e59b9c00bb8191cf30008091382312ea511953c0
GustavoSantanaS <gustavossantanasantos@gmail.com>
2024-09-18 17:17:12 -0300

    validar_email.py

--- tema 13/validar_email.py
+++ tema 13/validar_email.py
@@ -1,6 +1,6 @@
 e = input('Digite seu email: ')
 
-if e == "Gabrielalves@gmail.com":
+if e == "Gustavosantana@gmail.com":
     print('Email validado corretamente!')
 else:
     print('Email incorreto, tente novamente.')
