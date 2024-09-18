fcb0ebd1e806f24e905a9eae4b920c0f8e6f7644
GustavoSantanaS <gustavossantanasantos@gmail.com>
2024-09-18 17:11:13 -0300

    Add files via upload

---
+++ tema 13/validar_email.py
@@ -0,0 +1,6 @@
+e = input('Digite seu email: ')
+
+if e == "Gabrielalves@gmail.com":
+    print('Email validado corretamente!')
+else:
+    print('Email incorreto, tente novamente.')
---
+++ tema 13/validar_nome.py
@@ -0,0 +1,6 @@
+n = input('Digite seu nome: ')
+
+if n == "Gabriel":
+    print('Nome validado corretamente!')
+else:
+    print('Nome incorreto, tente novamente.')
