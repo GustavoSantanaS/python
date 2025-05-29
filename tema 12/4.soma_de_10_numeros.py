7844929cd9f77fb076e6a1021552cea3a87f90c7
GustavoSantanaS <gustavossantanasantos@gmail.com>
2025-05-29 15:52:48 -0300

    Update and rename 4.soma de 10 numeros.py to 4.soma_de_10_numeros.py

--- tema 12/4.soma_de_10_numeros.py
+++ tema 12/4.soma_de_10_numeros.py
@@ -3,8 +3,8 @@ soma = 0
 
 # Loop que le os numeros inteiros e entrada de dados
 for i in range(10):
-    n = int(input("Digite um número inteiro: "))
-    soma = soma + n 
+    numero = int(input("Digite um número inteiro: "))
+    soma = soma + numero 
 
 #mostra soma de todos os números lidos
 print("A soma de todos os números lidos é:", soma)
